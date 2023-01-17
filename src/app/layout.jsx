64efc976ff42c8f97711a28b26e912e705b3ddef
kushagrakrish <krishna619kk@gmail.com>
2023-01-17 17:02:24 +0530

    my first commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,18 @@
-import './globals.css'
+import "./globals.css";
+import Navbar from "./Navbar";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/pages/Cars.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cars = () => {
+  return <div>This is a car components</div>;
+};
+
+export default Cars;

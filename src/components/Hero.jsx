e3243308cda092cbc89d60fbf56fcf7bac965c5e
kushagrakrish <krishna619kk@gmail.com>
2023-01-17 18:19:08 +0530

    responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <>
-      <div className='flex items-center justify-between bg-black h-screen px-32'>
+      <div className='flex items-center justify-between bg-black h-screen px-32 flex-wrap-reverse max-[1450px]:justify-center'>
         <div className=''>
           <h1 className='text-5xl text-gray-600 font-bold mb-8'>Hello,there</h1>
           <h2 className='text-xl text-gray-400 w-[30rem]'>
